ba24f10d88e15d0948f75d484c7eee2e4ec453b2
AlaaAbdelmotlep <52351170+AlaaAbdelmotlep@users.noreply.github.com>
2022-04-06 22:40:36 +0200

    Home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import "./App.css";
 import Wishlist from "./components/wishList/wishListContent/Wishlist";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import HomePage from "./components/HomePage/HomePage";
+import ProductPage from "./components/ProductPage/ProductPage";
+import ProductSlider from "./components/ProductSlider/ProductSlider";
+
+import "swiper/css/bundle";
 
 function App() {
   return (
-    
-    <div className="App">
-      <Navbar />
+    <div className="">
+      <ProductPage />
+      {/* <Navbar />
       <Hero />
-        <Switch>
-          <Route exact path="/">
-              <h1>This is home page</h1>
-          </Route>
-          <Route  path="/whishlist">
-                <Wishlist />
-          </Route>
-          </Switch>         
-        
-      <Footer />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/whishlist">
+          <Wishlist />
+        </Route> */}
+      {/* <Route path="/product-page">
+          <ProductPage />
+        </Route> */}
+      {/* <Route path="/product-slider">
+          <ProductSlider />
+        </Route>  */}
+      {/* </Switch> */}
+
+      {/* <Footer /> */}
     </div>
   );
 }
