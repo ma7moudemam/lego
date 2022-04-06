ef99eb1e2bfdc3164e9850cc28a5935de108ac5c
m7moud emam <m7moud_emam@hotmail.com>
2022-04-06 13:49:46 +0200

    Product Cart

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import Hero from "./components/Hero/Hero";
+import ProductCart from "./components/ProductCart/ProductCart";
 
 function App() {
   return (
     
     <div className="App">
       <Navbar />
+      <ProductCart/>
       <Hero />
         <Switch>
           <Route exact path="/">
