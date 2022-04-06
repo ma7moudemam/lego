4bf5c3ae4932258b402dabbbeb83f46b264d5a68
Karim <karim.alaa.shahin@gmail.com>
2022-04-06 15:02:37 +0200

    utility bar updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import "./App.css";
 import Wishlist from "./components/wishList/wishListContent/Wishlist";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import ProductCart from "./components/ProductCart/ProductCart";
+import Utility from "./components/Utilitybar/Utility";
 
 function App() {
-  return (
-    
-    <div className="App">
-      <Navbar />
-      <ProductCart/>
-      <Hero />
-        <Switch>
-          <Route exact path="/">
-              <h1>This is home page</h1>
-          </Route>
-          <Route  path="/whishlist">
-                <Wishlist />
-          </Route>
-          </Switch>         
-        
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Utility />
+			<Navbar />
+			<Hero />
+			<ProductCart />
+			<Switch>
+				<Route exact path="/">
+					<h1>This is home page</h1>
+				</Route>
+				<Route path="/whishlist">
+					<Wishlist />
+				</Route>
+			</Switch>
+
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
