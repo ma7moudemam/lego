0bf2e83cac71f488fd6c595a705e9772c225e87f
Karim <karim.alaa.shahin@gmail.com>
2022-04-06 03:11:46 +0200

    Hero Component Updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
-import './App.css';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
 
 function App() {
-  return (
-    <div className="App">
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Hero />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
