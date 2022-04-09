f070ff791d8fe173159dfbbef1c1dc102878486f
romisaa-samir <romisaa.samir.mahmoud@gmail.com>
2022-04-09 15:01:15 +0200

    adding redux

--- src/index.js
+++ src/index.js
@@ -8,13 +8,18 @@ import "./assets/fonts/Cera Pro Medium.otf";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+//Redux
+import {store} from './Redux/store'
+import {Provider} from 'react-redux'
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
