0400008d29a22415c35e7e3498ccc2fee2f2cc54
Karim <karim.alaa.shahin@gmail.com>
2022-04-16 06:48:31 +0200

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
-import "./App.css";
-import Wishlist from "./components/wishList/wishListContent/Wishlist";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AccordionProduct from "./components/AccordionProduct/AccordionProduct";
-import MyBag from "./components/Bag";
-import ProductCart from "./components/ProductCart/ProductCart";
-import Personal from "./components/personal&address/Personal";
-import HomePage from "./components/HomePage/HomePage";
-import ProductCard from "./components/ProductsCard/ProductCard";
-import SwiperSlider from "./components/SwiperSlider/SwiperSlider";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Login from "./components/Login/Login";
-import Signup from "./components/Signup/Signup";
-import "./App.css";
-import AccountDetails from "./components/AccountDetails/AccountDetails";
-import AccountInformation from "./components/AccountDetails/AccountInformation";
-import AccountSecurity from "./components/AccountDetails/AccountSecurity";
-import DeleteAccount from "./components/AccountDetails/DeleteAccount";
 import { useEffect, useState } from "react";
 import { getUserCart, updateUserCart } from "./network/cartAPI";
 import { useSelector, useDispatch } from "react-redux";
 import { initUserCart } from "./Redux/Actions/cartActions";
-import Layout from "./components/Layout/Layout";
-import Checkout from "./components/Checkout/Checkout";
-import Success from "./components/Success/Success";
-import Canceled from "./components/Canceled/Canceled";
-import NotFound from "./components/NotFound/NotFound";
-import Navbar from "./components/Navbar/Navbar";
-import ContactUs from "./components/Contact-us/ContactUs";
+import "./App.css";
 
-import MyOrder from "./components/Myorder/MyOrder";
-import WishListContent from "./components/wishList/wishListContent/WishlistContent";
-
+const AccordionProduct = lazy(() => import("./components/AccordionProduct/AccordionProduct"));
+const MyBag = lazy(() => import("./components/Bag"));
+const ProductCart = lazy(() => import("./components/ProductCart/ProductCart"));
+const Personal = lazy(() => import("./components/personal&address/Personal"));
+const HomePage = lazy(() => import("./components/HomePage/HomePage"));
+const ProductCard = lazy(() => import("./components/ProductsCard/ProductCard"));
+const SwiperSlider = lazy(() => import("./components/SwiperSlider/SwiperSlider"));
+const Dashboard = lazy(() => import("./components/Dashboard/Dashboard"));
+const Login = lazy(() => import("./components/Login/Login"));
+const Signup = lazy(() => import("./components/Signup/Signup"));
+const AccountDetails = lazy(() => import("./components/AccountDetails/AccountDetails"));
+const AccountInformation = lazy(() => import("./components/AccountDetails/AccountInformation"));
+const AccountSecurity = lazy(() => import("./components/AccountDetails/AccountSecurity"));
+const DeleteAccount = lazy(() => import("./components/AccountDetails/DeleteAccount"));
+const Layout = lazy(() => import("./components/Layout/Layout"));
+const Checkout = lazy(() => import("./components/Checkout/Checkout"));
+const Success = lazy(() => import("./components/Success/Success"));
+const Canceled = lazy(() => import("./components/Canceled/Canceled"));
+const NotFound = lazy(() => import("./components/NotFound/NotFound"));
+const Navbar = lazy(() => import("./components/Navbar/Navbar"));
+const ContactUs = lazy(() => import("./components/Contact-us/ContactUs"));
+const MyOrder = lazy(() => import("./components/Myorder/MyOrder"));
+const WishListContent = lazy(() => import("./components/wishList/wishListContent/WishlistContent"));
+const Wishlist = lazy(() => import("./components/wishList/wishListContent/Wishlist"));
 
 function App() {
 	const dispatch = useDispatch();
 	const cart = useSelector((store) => store.cart);
 	const [initCart, setInitCart] = useState(false);
-	const [token, setToken] = useState(localStorage.getItem("token"))
+	const [token, setToken] = useState(localStorage.getItem("token"));
 
 	useEffect(() => {
-		if(token) {
+		if (token) {
 			if (initCart) {
 				updateUserCart({ ...cart })
 					.then((res) => console.log("===>UPDATE CART ", res.data))
@@ -50,123 +49,133 @@ function App() {
 					.then((res) => dispatch(initUserCart(res.data)))
 					.catch((err) => console.log("==>ERROR CART ,", err));
 			}
-		}	
-		
-	}, [cart, token]);				
+		}
+	}, [cart, token]);
 
 	return (
 		<div className="App">
 			<Router>
-				<Routes>
-					<Route
-						path="/mybag"
-						element={
-							<Layout>
-								<MyBag />
-							</Layout>
-						}
-					/>
-					<Route
-						path="/"
-						element={
-							<Layout>
-								<HomePage />
-							</Layout>
-						}
-					/>
-					<Route
-						path="/home"
-						element={
-							<Layout>
-								<HomePage />
-							</Layout>
-						}
-					/>
-					<Route
-						path="/whishlist"
-						element={
-							<Layout>
-								<Wishlist />
-							</Layout>
-						}
-					>
-    			 		<Route  path="personal" element={<Personal />} />
-						<Route path="myorder" element={<MyOrder />} />
-						<Route path="whishlist" element={<WishListContent />} />
-					</Route>
-					<Route path="/login" element={<Login />} />
-					<Route path="/signup" element={<Signup />} />
-					<Route
-						path="/shop-now"
-						element={
-							<Layout>
+				<Suspense
+					fallback={
+						<div className="loader">
+							<h1>Our website is made with love</h1>
+							<div className="lds-heart">
+								<div></div>
+							</div>
+						</div>
+					}
+				>
+					<Routes>
+						<Route
+							path="/mybag"
+							element={
+								<Layout>
+									<MyBag />
+								</Layout>
+							}
+						/>
+						<Route
+							path="/"
+							element={
+								<Layout>
+									<HomePage />
+								</Layout>
+							}
+						/>
+						<Route
+							path="/home"
+							element={
+								<Layout>
+									<HomePage />
+								</Layout>
+							}
+						/>
+						<Route
+							path="/whishlist"
+							element={
+								<Layout>
+									<Wishlist />
+								</Layout>
+							}
+						>
+							<Route path="personal" element={<Personal />} />
+							<Route path="myorder" element={<MyOrder />} />
+							<Route path="whishlist" element={<WishListContent />} />
+						</Route>
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<Signup />} />
+						<Route
+							path="/shop-now"
+							element={
+								<Layout>
+									<>
+										<ProductCard />
+										<AccordionProduct />
+									</>
+								</Layout>
+							}
+						/>
+						<Route
+							path="/card"
+							element={
+								<Layout>
+									<>
+										<ProductCart />
+										<SwiperSlider />
+									</>
+								</Layout>
+							}
+						/>
+						<Route
+							path="/my-account"
+							element={
 								<>
-								<ProductCard />
-								<AccordionProduct />
+									<Navbar />
+									<Personal />
 								</>
-							</Layout>
-						}
-					/>
-					<Route
-						path="/card"
-						element={
-							<Layout>
-								<>
-								<ProductCart />
-								<SwiperSlider />
-								</>
-							</Layout>
-						}
-					/>
-					<Route
-						path="/my-account"
-						element={
-							<>
-								<Navbar />
-								<Personal />
-							</>
-						}
-					/>
-					<Route path="/details" element={<AccountDetails />} />
-					<Route path="/info" element={<AccountInformation />} />
-					<Route path="/security" element={<AccountSecurity />} />
-					<Route path="/delete-account" element={<DeleteAccount />} />
-					<Route path="/dashboard" element={<Dashboard />} />
-					<Route
-						path="/checkout"
-						element={
-							<Layout>
-								<Checkout />
-							</Layout>
-						}
-					/>
-					<Route
-						path="/success"
-						element={
-							<Layout>
-								<Success />
-							</Layout>
-						}
-					/>
-					<Route
-						path="/canceled"
-						element={
-							<Layout>
-								<Canceled />
-							</Layout>
-						}
-					/>
-					<Route
-						path="/contact-us"
-						element={
-							<Layout>
-								<ContactUs />
-							</Layout>
-						}
-					/>
-					<Route path="/about-us" element={<NotFound />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
+							}
+						/>
+						<Route path="/details" element={<AccountDetails />} />
+						<Route path="/info" element={<AccountInformation />} />
+						<Route path="/security" element={<AccountSecurity />} />
+						<Route path="/delete-account" element={<DeleteAccount />} />
+						<Route path="/dashboard" element={<Dashboard />} />
+						<Route
+							path="/checkout"
+							element={
+								<Layout>
+									<Checkout />
+								</Layout>
+							}
+						/>
+						<Route
+							path="/success"
+							element={
+								<Layout>
+									<Success />
+								</Layout>
+							}
+						/>
+						<Route
+							path="/canceled"
+							element={
+								<Layout>
+									<Canceled />
+								</Layout>
+							}
+						/>
+						<Route
+							path="/contact-us"
+							element={
+								<Layout>
+									<ContactUs />
+								</Layout>
+							}
+						/>
+						<Route path="/about-us" element={<NotFound />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</Suspense>
 			</Router>
 		</div>
 	);
