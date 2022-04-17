50e1304df94cd69f72539308ed962e8be798f435
Karim <karim.alaa.shahin@gmail.com>
2022-04-17 12:44:41 +0200

    update..

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import legoAccountLogo from "./../../assets/imgs/LEGOAccount-Logo.svg";
 import close from "./../../assets/imgs/close.svg";
 import legoLarge from "./../../assets/imgs/lego(1)large.png";
@@ -14,6 +14,7 @@ import "./Login.css";
 import { useNavigate, NavLink } from "react-router-dom";
 
 function Login() {
+	const [isLoading, setIsLoading] = useState(false);
 	let navigate = useNavigate();
 	const formik = useFormik({
 		initialValues: {
@@ -25,14 +26,16 @@ function Login() {
 			password: Yup.string().required("password is required"),
 		}),
 		onSubmit: (values) => {
+			setIsLoading(true);
 			axios
 				.post("http://localhost:8080/login", values)
 				.then((res) => {
-					//res.data
 					localStorage.setItem("token", res.data.token);
+					localStorage.setItem("refreshToken", res.data.refreshToken);
+					setIsLoading(false);
+					navigate("/home");
 				})
 				.catch((err) => console.log(err));
-			navigate("/home");
 		},
 	});
 	// const submitLogin = (event) => {
@@ -40,70 +43,89 @@ function Login() {
 	//   console.log(event);
 	// };
 	return (
-		<div className="auth-wrapper">
-			<div className="login-container">
-				<div className="header">
-					<div className="title">
-						<img src={legoAccountLogo} alt="Lego Logo" />
-					</div>
-					<div className="close">
-						<NavLink to="/home">
-							<img src={close} alt="close button" />
-						</NavLink>
+		<>
+			{isLoading && (
+				<div className="inner-loader">
+					<h1>Logging you in please hold</h1>
+					<div className="lds-ring">
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
 					</div>
 				</div>
-				<div className="logo">
-					<img src={legoLarge} alt="" />
-				</div>
-				<Box
-					component="form"
-					className="lego-form"
-					noValidate
-					autoComplete="off"
-					onSubmit={formik.handleSubmit}
-				>
-					<FormControl sx={{ width: "100%", px: 3, mb: 2 }}>
-						<TextField
-							helperText={formik.touched.email && formik.errors.email ? `${formik.errors.email}` : null}
-							error={formik.touched.email && formik.errors.email ? true : false}
-							id="demo-helper-text-misaligned"
-							label="Email"
-							{...formik.getFieldProps("email")}
-						/>
-					</FormControl>
-					<FormControl sx={{ width: "100%", px: 3, mb: 2 }}>
-						<TextField
-							helperText={
-								formik.touched.password && formik.errors.password ? `${formik.errors.password}` : null
-							}
-							error={formik.touched.password && formik.errors.password ? true : false}
-							id="standard-error-helper-text"
-							label="Password"
-							type="password"
-							{...formik.getFieldProps("password")}
-						/>
-					</FormControl>
+			)}
+			{!isLoading && (
+				<div className="auth-wrapper">
+					<div className="login-container">
+						<div className="header">
+							<div className="title">
+								<img src={legoAccountLogo} alt="Lego Logo" />
+							</div>
+							<div className="close">
+								<NavLink to="/home">
+									<img src={close} alt="close button" />
+								</NavLink>
+							</div>
+						</div>
+						<div className="logo">
+							<img src={legoLarge} alt="" />
+						</div>
+						<Box
+							component="form"
+							className="lego-form"
+							noValidate
+							autoComplete="off"
+							onSubmit={formik.handleSubmit}
+						>
+							<FormControl sx={{ width: "100%", px: 3, mb: 2 }}>
+								<TextField
+									helperText={
+										formik.touched.email && formik.errors.email ? `${formik.errors.email}` : null
+									}
+									error={formik.touched.email && formik.errors.email ? true : false}
+									id="demo-helper-text-misaligned"
+									label="Email"
+									{...formik.getFieldProps("email")}
+								/>
+							</FormControl>
+							<FormControl sx={{ width: "100%", px: 3, mb: 2 }}>
+								<TextField
+									helperText={
+										formik.touched.password && formik.errors.password
+											? `${formik.errors.password}`
+											: null
+									}
+									error={formik.touched.password && formik.errors.password ? true : false}
+									id="standard-error-helper-text"
+									label="Password"
+									type="password"
+									{...formik.getFieldProps("password")}
+								/>
+							</FormControl>
 
-					<div className="remember-me">
-						<FormControlLabel control={<Checkbox />} label="Remember me" />
+							<div className="remember-me">
+								<FormControlLabel control={<Checkbox />} label="Remember me" />
+							</div>
+							<p className="logout-advice">
+								Remember to log out afterwards if you are using a shared computer, for example in a
+								library or school.
+							</p>
+							<button type="submit" className="login-button">
+								Log in
+							</button>
+						</Box>
+						<div className="forgetting">
+							<a href="#test">Forgot username?</a> |<a href="#test">Forgot password</a>
+						</div>
+						<div className="go-signup">
+							<p>Don't have a LEGO® Account?</p>
+							<NavLink to="/signup">Create account</NavLink>
+						</div>
 					</div>
-					<p className="logout-advice">
-						Remember to log out afterwards if you are using a shared computer, for example in a library or
-						school.
-					</p>
-					<button type="submit" className="login-button">
-						Log in
-					</button>
-				</Box>
-				<div className="forgetting">
-					<a href="#test">Forgot username?</a> |<a href="#test">Forgot password</a>
 				</div>
-				<div className="go-signup">
-					<p>Don't have a LEGO® Account?</p>
-					<NavLink to="/signup">Create account</NavLink>
-				</div>
-			</div>
-		</div>
+			)}
+		</>
 	);
 }
 
